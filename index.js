b4544f892c75db6dccd78ed564277e1026dcf566
terry623 <terry92516@gmail.com>
2018-07-02 22:58:29 +0800

    change back

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const { ConsoleBot } = require('bottender');
+const { LineBot } = require('bottender');
+const { createServer } = require('bottender/express');
 
-const bot = new ConsoleBot({ fallbackMethods: true });
-
-bot.onEvent(async context => {
-  await context.sendText('Hello World Console');
+const bot = new LineBot({
+  channelSecret: 'a37375a40e56d4db2b0b73a53f2c8353',
+  accessToken:
+    'BO14M3aCS0TSVMlegbNshvEf+0s64FktENYaQE830oQnIAZlydlO4JvKd+bRo4sc/5hZI84xRyeCaMPp459v3QXSMPN7Ahx8qvsWMBu89xEhURKhRRzO1b90LU+2GYyepv/9n5HSbGi9OluomjnWLgdB04t89/1O/w1cDnyilFU=',
 });
 
-bot.createRuntime();
+bot.onEvent(async context => {
+  await context.sendText('Hello World');
+});
+
+const server = createServer(bot);
+
+server.listen(5000, () => {
+  console.log('server is running on 5000 port...');
+});
