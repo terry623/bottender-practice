f0d1b9430074af89bf7fc4752e38e3aaebee8af7
terry623 <terry92516@gmail.com>
2018-07-03 11:33:20 +0800

    nickname

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import { askNickname } from './helper';
-
 const { LineBot, LineHandler } = require('bottender');
 const { createServer } = require('bottender/express');
 const { Line } = require('messaging-api-line');
@@ -16,6 +14,17 @@ bot.setInitialState({
   asking: false,
   nickname: null,
 });
+
+async function askNickname(context) {
+  if (context.state.asking) {
+    context.setState({ nickname: context.event.text, asking: false });
+    await context.sendText(`Hello ${context.state.nickname}`);
+  } else {
+    context.resetState();
+    context.setState({ asking: true });
+    await context.sendText("Hi, what's your nickname?");
+  }
+}
 
 const handler = new LineHandler()
   .onText(/yo/i, async context => {
