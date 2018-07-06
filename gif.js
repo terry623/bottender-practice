d9c58d0cd1bf1f2b7ffd15f86eeffff82e162867
kpman <s92f002@hotmail.com>
2018-07-06 18:07:43 +0800

    Refactor promise

--- gif.js
+++ gif.js
@@ -2,40 +2,30 @@ const GphApiClient = require('giphy-js-sdk-core');
 
 const client = GphApiClient('WCyzh8Jtc3RNEOwPPELQy5QGhuHUGpwJ');
 
-const search = query =>
-  new Promise((resolve, reject) => {
-    const photoLimit = 20;
-    const option = {
-      q: query,
-      limit: photoLimit,
-    };
+const search = async query => {
+  const photoLimit = 20;
+  const option = {
+    q: query,
+    limit: photoLimit,
+  };
+  const chooseImage = Math.floor(Math.random() * photoLimit);
 
-    client
-      .search('gifs', option)
-      .then(response => {
-        const chooseImage = Math.floor(Math.random() * photoLimit);
-        const { original: origin, fixed_width_small: preview } = response.data[
-          chooseImage
-        ].images;
+  const { data } = await client.search('gifs', option);
+  const { original: origin, fixed_width_small: preview } = data[
+    chooseImage
+  ].images;
 
-        resolve([origin.gif_url, preview.gif_url]);
-      })
-      .catch(err => reject(err));
-  });
+  return [origin.gif_url, preview.gif_url];
+};
 
-const random = () =>
-  new Promise((resolve, reject) => {
-    client
-      .random('gifs', {})
-      .then(response => {
-        const {
-          original: origin,
-          fixed_width_small: preview,
-        } = response.data.images;
+const random = async () => {
+  const {
+    data: { images },
+  } = await client.random('gifs', {});
 
-        resolve([origin.gif_url, preview.gif_url]);
-      })
-      .catch(err => reject(err));
-  });
+  const { original: origin, fixed_width_small: preview } = images;
+
+  return [origin.gif_url, preview.gif_url];
+};
 
 module.exports = { search, random };
